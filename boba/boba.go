30b6b78570949093cb16536f03e61402213d3295
Corki Udyr <corkiudy@un-c1.kzn.21-school.ru>
2022-09-01 17:50:33 +0300

    add_struct

--- boba/boba.go
+++ boba/boba.go
@@ -2,6 +2,18 @@
 package boba
 
 import "fmt"
+
+type Person struct {
+	Name        string
+	Age         int
+	HomeAddress Address
+}
+type Address struct {
+	Town         string
+	street       string
+	NumberOfTown int
+}
+
 // boba doc paramenrs and comments
 func Boba() {
 	fmt.Println("aaa boba! ")
