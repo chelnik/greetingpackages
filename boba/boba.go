3b61e971af796793cff7bf13b4cc35d9d84ef6c4
Corki Udyr <corkiudy@un-c1.kzn.21-school.ru>
2022-09-01 17:26:35 +0300

    doc

--- boba/boba.go
+++ boba/boba.go
@@ -1,7 +1,12 @@
+// this is my doc
 package boba
 
 import "fmt"
-
+// boba doc paramenrs and comments
 func Boba() {
-	fmt.Println("aaa boba!")
+	fmt.Println("aaa boba! ")
+}
+
+func Biba() {
+	fmt.Println("aaa biba! ")
 }
